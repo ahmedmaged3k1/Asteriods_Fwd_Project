82776c22c57ee6e78bf7b89a5fae123f2a9271ed
ahmedmaged3k1 <60134186+ahmedmaged3k1@users.noreply.github.com>
2022-08-22 20:43:06 +0200

    Details Fragment Done

--- app/src/main/java/com/udacity/asteroidradar/main/AsteroidsRecyclerViewAdapter.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/AsteroidsRecyclerViewAdapter.kt
@@ -1,13 +1,16 @@
 package com.udacity.asteroidradar.main
 
+import android.content.ContentValues.TAG
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-
 import com.udacity.asteroidradar.StartDate
 import com.udacity.asteroidradar.databinding.AsteroidItemBinding
+import com.udacity.asteroidradar.test.AsteroidPracable
 
 
 val diffCallback = object : DiffUtil.ItemCallback<StartDate>() {
@@ -16,12 +19,13 @@ val diffCallback = object : DiffUtil.ItemCallback<StartDate>() {
     }
 
     override fun areContentsTheSame(oldItem: StartDate, newItem: StartDate): Boolean {
-        return oldItem.id == newItem.id
+        return false
 
 
     }
 }
 
+@Suppress("DEPRECATION")
 class AsteroidsRecyclerViewAdapter :
     ListAdapter<StartDate, AsteroidsRecyclerViewAdapter.AsteroidsViewHolder>(diffCallback) {
 
@@ -44,8 +48,26 @@ class AsteroidsRecyclerViewAdapter :
 
         }
 
+        init {
+            binding.constraintLayout.setOnClickListener {
+                Log.d("TAG", "message ${getItem(position).toString()} ")
+                Log.d(TAG, "message ${getItem(position).relative_velocity}: ")
+                val asteroid = AsteroidPracable(
+                    getItem(position).close_approach_data[0].close_approach_date_full,
+                    getItem(position).absolute_magnitude_h.toString(),
+                    getItem(position).close_approach_data[0].relative_velocity.kilometers_per_hour.toString(),
+                    getItem(position).estimated_diameter.kilometers.estimated_diameter_max.toString(),
+                    getItem(position).close_approach_data[0].miss_distance.kilometers.toString(),
+                    getItem(position).is_potentially_hazardous_asteroid
+                )
+                val action = MainFragmentDirections.actionShowDetail(asteroid)
 
+                binding.root.findNavController().navigate(action)
+
+            }
+        }
     }
+
 
     fun from(parent: ViewGroup): AsteroidsViewHolder {
         val inflater = LayoutInflater.from(parent.context)
